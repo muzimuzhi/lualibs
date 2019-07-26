71244c57ae0b12788865a3ae254bd5d5371bec90
Ulrike Fischer <fischer@troubleshooting-tex.de>
2019-07-26 22:55:14 +0200

    enable tagging

---
+++ README.md
@@ -0,0 +1,42 @@
+# The Lualibs Package
+
+![Version: 2.6702](https://img.shields.io/badge/current_version-2.6702-blue.svg?style=flat-square)
+![Date: 2019-07-24](https://img.shields.io/badge/date-2019--07--24-blue.svg?style=flat-square)
+[![License: GNU GPLv2](https://img.shields.io/badge/license-GNU_GPLv2-blue.svg?style=flat-square)](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+
+
+Lualibs is a collection of Lua modules useful for general programming. 
+It is used by the luaotfload package.
+
+This work is based on Lua modules shipped with ConTeXt, to make them available
+for use independent of ConTeXt.
+
+This package has been developed by the LuaLaTeX development team on
+<http://github.com/lualatex/lualibs>. 
+
+The current version has been build by Ulrike Fischer on
+<https://github.com/u-fischer/lualibs> from context 2019-07-24. 
+
+Please see the documentation lualibs.pdf for more information.
+
+## Development versions
+
+The ongoing development is in branches referring to the dates of the import of the context files. 
+ 
+## Support
+[![GitHub issues](https://img.shields.io/badge/github-issues-blue.svg?style=flat-square)](https://github.com/u-fischer/luaotfload/issues) 
+[![mailing list](https://img.shields.io/badge/mailing_list-lualatex--dev-blue.svg?style=flat-square)](https://www.tug.org/mailman/listinfo/lualatex-dev) 
+
+
+Issues can be reported at the issue tracker. 
+
+## License
+
+As a derived work of Context, this work and the derived files are under the
+GNU GPL v.2.0 license.
+
+The full text of the license is included in the file LICENSE.
+
+The files taken from Context (lualibs-*.lua) are copyright PRAGMA ADE / ConTeXt
+Development Team.
+
--- build.lua
+++ build.lua
@@ -1,8 +1,11 @@
 -- Build script for lualibs
-packageversion="2.66"
-packagedate="2019-07-04"
+packageversion= "2.6702"
+packagedate= "2019-07-24"
 
+module   = "lualibs"
+ctanpkg  = "lualibs"
 
+-- get personal data
 local ok, mydata = pcall(require, "ulrikefischerdata.lua")
 if not ok then
   mydata= {email="XXX",github="XXX",name="XXX"}
@@ -10,62 +13,129 @@ end
 
 print(mydata.email)
 
-module   = "lualibs"
-ctanpkg  = "lualibs"
-
 uploadconfig = {
-  pkg     = ctanpkg,
-  version = "v"..packageversion.." "..packagedate,
-  author  = "Philipp Gesang; Élie Roux",
-  license = "gpl2",
-  summary = "Additional Lua functions for LuaTeX macro programmers",
-  ctanPath = "/macros/luatex/generic/lualibs",
+  pkg        = ctanpkg,
+  version    = "v"..packageversion.." "..packagedate,
+  author     = "Philipp Gesang; Élie Roux",
+  license    = "gpl2",
+  summary    = "Additional Lua functions for LuaTeX macro programmers",
+  ctanPath   = "/macros/luatex/generic/lualibs",
   repository = mydata.github .. "lualibs",
   bugtracker = mydata.github .. "lualibs/issues",
   support    = mydata.github .. "lualibs/issues",
   uploader   = mydata.name,
   email      = mydata.email, 
-  update   = true ,
-  topic=    {"luatex","lua-supp"},
-  note     = [[Uploaded automatically by l3build... Description and authors unchanged]],
+  update     = true ,
+  topic      = {"luatex","lua-supp"},
+  note       = [[Uploaded automatically by l3build... Description and authors unchanged]],
   description=[[Lualibs is a collection of Lua modules useful for general programming. 
   The bundle is based on lua modules shipped with ConTeXt, and made available in this bundle for use independent of ConTeXt.]],
   announcement="This version syncs the files with the ConTeXt files from "..packagedate.."."              
 }
 
+-- check (currently no tests)
+stdengine    = "luatex"
 checkengines = {"luatex"}
 checkruns    = 3
-packtdszip   =true
 
+---------------------------------------------
+-- l3build settings for CTAN/install target
+---------------------------------------------
+
+packtdszip   =true
+-- sourcefiledir (default .)
+-- docfiledir    (default .)
+
+-- set texmfhome for local installation.
+-- the files go to the side-by-side folder of luaotfload
+-- to make it easier to run the tests there and to have a complete
+-- set there for users
+          
+options["texmfhome"] = "../luaotfload/texmf"           
+
+-------------------
 -- documentation
+-------------------
 
 typesetexe   = "lualatex"
-unpackfiles  =  {"*.dtx"}
-docfiles     = {"README","NEWS","LICENSE"}
+unpackfiles  = {"*.dtx"}
 
--- automatic tagging: perhaps later ...
+textfiles = 
+ {
+  "LICENSE",
+  "NEWS",
+  "CTANREADME.md",
+ }
+
+---------------------
+-- installation
+---------------------
+tdsroot = "luatex"
 
 installfiles = {
                 "**/lualibs-*.lua",
                 "lualibs.lua",
                }  
 
--- ctan setup
-tdsroot = "luatex"
-textfiles    = {"README","NEWS","LICENSE"}
-
-               
 sourcefiles  = {
                 "*.dtx",
                 "lualibs-*.lua",
                }
 
+-----------------------------
+-- l3build settings for tags:
+-----------------------------
+tagfiles = {
+            "CTANREADME.md",
+            "README.md",
+            "lualibs.dtx",
+            }
 
-
--- set texmfhome for local installation.
--- the files go to the side-by-side folder of luaotfload
--- to make it easier to run the tests there.          
-options["texmfhome"] = "../luaotfload/texmf"              
+function update_tag (file,content,tagname,tagdate)
+ tagdate = string.gsub (packagedate,"-", "/")
+ if string.match (file, "%.dtx$" ) then
+  content = string.gsub (content,  
+                         "%d%d%d%d/%d%d/%d%d [a-z]+%d%.%d+",
+                         tagdate.." v"..packageversion)
+  content = string.gsub (content,  
+                         "%d%d%d%d%-%d%d%-%d%d [a-z]+%d%.%d+",
+                         packagedate.." v"..packageversion)
+  content = string.gsub (content,  
+                         '(version%s*=%s*")%d%.%d+(",%s*--TAGVERSION)',
+                         "%1"..packageversion.."%2")
+  content = string.gsub (content,  
+                         '(date%s*=%s*")%d%d%d%d%-%d%d%-%d%d(",%s*--TAGDATE)',
+                         "%1"..packagedate.."%2")                                                  
+  return content                         
+ elseif string.match (file, "^README.md$") then
+   content = string.gsub (content,  
+                         "Version: %d%.%d+",
+                         "Version: " .. packageversion )
+   content = string.gsub (content,  
+                         "version%-%d%.%d+",
+                         "version-" .. packageversion ) 
+   content = string.gsub (content,  
+                         "for %d%.%d+",
+                         "for " .. packageversion ) 
+   content = string.gsub (content,  
+                         "%d%d%d%d%-%d%d%-%d%d",
+                         packagedate )
+   local imgpackagedate = string.gsub (packagedate,"%-","--")                          
+   content = string.gsub (content,  
+                         "%d%d%d%d%-%-%d%d%-%-%d%d",
+                         imgpackagedate)                                                                                                     
+   return content
+ elseif string.match (file, "CTANREADME.md$") then
+   content = string.gsub (content,  
+                         "VERSION: %d%.%d+",
+                         "VERSION: " .. packageversion )
+   content = string.gsub (content,  
+                         "DATE: %d%d%d%d%-%d%d%-%d%d",
+                         "DATE: " .. packagedate )                                                                          
+   return content                               
+ end
+ return content
+ end   
 
 
 
